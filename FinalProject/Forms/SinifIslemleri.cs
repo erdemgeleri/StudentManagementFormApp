106191ba27ad4b48e531a27608a45da2e4cd36ab
erdemgeleri <erdem_geleri@hotmail.com>
2024-12-30 23:43:46 +0300

    command blocks

--- FinalProject/Forms/SinifIslemleri.cs
+++ FinalProject/Forms/SinifIslemleri.cs
@@ -26,160 +26,160 @@ namespace FinalProject.Forms
 
         private void btnBul_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (var ctx = new FinalDBContext())
-                {
-                    int sinifId;
-                    if (int.TryParse(tbSinifId.Text, out sinifId))
-                    {
-                        var sinif = ctx.Siniflar
-                                        .Where(s => s.SinifId == sinifId)
-                                        .FirstOrDefault();
+            //try
+            //{
+            //    using (var ctx = new FinalDBContext())
+            //    {
+            //        int sinifId;
+            //        if (int.TryParse(tbSinifId.Text, out sinifId))
+            //        {
+            //            var sinif = ctx.Siniflar
+            //                            .Where(s => s.SinifId == sinifId)
+            //                            .FirstOrDefault();
 
-                        if (sinif != null)
-                        {
-                            tbSinifAdi.Text = sinif.SinifAd;
-                            tbKontenjan.Text = sinif.Kontenjan.ToString();
-                            MessageBox.Show($"{sinif.SinifId} ID'li sınıf bulunmuştur");
-                        }
-                        else
-                        {
-                            MessageBox.Show("Kayıt bulunamadı");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            //            if (sinif != null)
+            //            {
+            //                tbSinifAdi.Text = sinif.SinifAd;
+            //                tbKontenjan.Text = sinif.Kontenjan.ToString();
+            //                MessageBox.Show($"{sinif.SinifId} ID'li sınıf bulunmuştur");
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("Kayıt bulunamadı");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
         }
 
         private void btnGuncelle_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (var ctx = new FinalDBContext())
-                {
-                    int sinifId;
-                    if (int.TryParse(tbSinifId.Text, out sinifId))
-                    {
-                        var sinif = ctx.Siniflar
-                                        .Where(s => s.SinifId == sinifId)
-                                        .FirstOrDefault();
+            //try
+            //{
+            //    using (var ctx = new FinalDBContext())
+            //    {
+            //        int sinifId;
+            //        if (int.TryParse(tbSinifId.Text, out sinifId))
+            //        {
+            //            var sinif = ctx.Siniflar
+            //                            .Where(s => s.SinifId == sinifId)
+            //                            .FirstOrDefault();
 
-                        if (sinif != null)
-                        {
-                            // Kontenjan kontrolü
-                            if (sinif.Kontenjan < 0)
-                            {
-                                MessageBox.Show("Kontenjan değeri geçersiz!");
-                                return;
-                            }
+            //            if (sinif != null)
+            //            {
+            //                // Kontenjan kontrolü
+            //                if (sinif.Kontenjan < 0)
+            //                {
+            //                    MessageBox.Show("Kontenjan değeri geçersiz!");
+            //                    return;
+            //                }
 
-                            // Güncelleme işlemi
-                            sinif.SinifAd = tbSinifAdi.Text;
-                            sinif.Kontenjan = int.Parse(tbKontenjan.Text);
-                            ctx.SaveChanges();
+            //                // Güncelleme işlemi
+            //                sinif.SinifAd = tbSinifAdi.Text;
+            //                sinif.Kontenjan = int.Parse(tbKontenjan.Text);
+            //                ctx.SaveChanges();
 
-                            MessageBox.Show("Sınıf başarıyla güncellendi!");
-                        }
-                        else
-                        {
-                            MessageBox.Show("Sınıf bulunamadı");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
-            }
+            //                MessageBox.Show("Sınıf başarıyla güncellendi!");
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("Sınıf bulunamadı");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            //}
         }
 
         private void btnSil_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (var ctx = new FinalDBContext())
-                {
-                    int sinifId;
-                    if (int.TryParse(tbSinifId.Text, out sinifId))
-                    {
-                        var sinif = ctx.Siniflar
-                                        .Where(s => s.SinifId == sinifId)
-                                        .FirstOrDefault();
+            //try
+            //{
+            //    using (var ctx = new FinalDBContext())
+            //    {
+            //        int sinifId;
+            //        if (int.TryParse(tbSinifId.Text, out sinifId))
+            //        {
+            //            var sinif = ctx.Siniflar
+            //                            .Where(s => s.SinifId == sinifId)
+            //                            .FirstOrDefault();
 
-                        if (sinif != null)
-                        {
-                            // Öğrenci kontrolü
-                            if (sinif.Ogrenciler.Count > 0)
-                            {
-                                MessageBox.Show("Bu sınıfta kayıtlı öğrenciler bulunmaktadır. Sınıf silinemez.");
-                                return;
-                            }
+            //            if (sinif != null)
+            //            {
+            //                // Öğrenci kontrolü
+            //                if (sinif.Ogrenciler.Count > 0)
+            //                {
+            //                    MessageBox.Show("Bu sınıfta kayıtlı öğrenciler bulunmaktadır. Sınıf silinemez.");
+            //                    return;
+            //                }
 
-                            ctx.Siniflar.Remove(sinif);
-                            ctx.SaveChanges();
+            //                ctx.Siniflar.Remove(sinif);
+            //                ctx.SaveChanges();
 
-                            MessageBox.Show("Sınıf başarıyla silindi.");
-                        }
-                        else
-                        {
-                            MessageBox.Show("Sınıf bulunamadı");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
-            }
+            //                MessageBox.Show("Sınıf başarıyla silindi.");
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("Sınıf bulunamadı");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Geçerli bir Sınıf ID'si giriniz!");
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            //}
         }
 
         private void btnEkle_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (var ctx = new FinalDBContext())
-                {
-                    string sinifAd = tbSinifAdi.Text.Trim();
-                    int kontenjan;
-                    if (string.IsNullOrEmpty(sinifAd) || !int.TryParse(tbKontenjan.Text, out kontenjan))
-                    {
-                        MessageBox.Show("Sınıf adı ve kontenjanı doğru giriniz.");
-                        return;
-                    }
+            //try
+            //{
+            //    using (var ctx = new FinalDBContext())
+            //    {
+            //        string sinifAd = tbSinifAdi.Text.Trim();
+            //        int kontenjan;
+            //        if (string.IsNullOrEmpty(sinifAd) || !int.TryParse(tbKontenjan.Text, out kontenjan))
+            //        {
+            //            MessageBox.Show("Sınıf adı ve kontenjanı doğru giriniz.");
+            //            return;
+            //        }
 
-                    var yeniSinif = new tblSiniflar
-                    {
-                        SinifAd = sinifAd,
-                        Kontenjan = kontenjan
-                    };
+            //        var yeniSinif = new tblSiniflar
+            //        {
+            //            SinifAd = sinifAd,
+            //            Kontenjan = kontenjan
+            //        };
 
-                    ctx.Siniflar.Add(yeniSinif);
-                    ctx.SaveChanges();
+            //        ctx.Siniflar.Add(yeniSinif);
+            //        ctx.SaveChanges();
 
-                    MessageBox.Show("Yeni sınıf başarıyla eklendi.");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
-            }
+            //        MessageBox.Show("Yeni sınıf başarıyla eklendi.");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            //}
         }
 
         private void btnEkle_Click_1(object sender, EventArgs e)
